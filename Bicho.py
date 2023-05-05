a0376c2ef8bd69e49723efb8ae020fcf3c54d289
ADRIÁN RODRÍGUEZ LÓPEZ <Adrian.Rodriguez18@alu.uclm.es>
2023-05-06 01:50:55 +0200

    Clase Bicho

--- Bicho.py
+++ Bicho.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-from Ente import Ente
 
-class Bicho(Ente):
+class Bicho():
     def __init__(self):
-        self.vidas = None
-        self.poder = None
+        self.vidas = 10     #Datos de ejemplo
+        self.poder = 1
+        self.modo = None
 
-    def actua(self, ):
-        pass
+    def esAgresivo(self):
+        return self.modo.esAgresivo
+    
+    def esPerezoso(self):
+        return self.modo.esPerezoso
 
+    def estaVivo(self): #Como es obvio no se ha tenido en cuenta la clase Estado
+        return self.vidas > 0
+    
+    def puedeActuar(self):
+        self.modo.actua(self)
+    
+    def quitarVidas(self, unNum):
+        self.vidas = self.vidas - unNum
