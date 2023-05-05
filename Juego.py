d782d6a1bd2291e7731ec472ebebe25c2bbfdbb6
ADRIÁN RODRÍGUEZ LÓPEZ <Adrian.Rodriguez18@alu.uclm.es>
2023-05-06 00:29:16 +0200

    Añadir 4habitaciones

--- Juego.py
+++ Juego.py
@@ -32,6 +32,39 @@ class Juego:
         habitacion2.ponerOeste(self.fabricarPared())
         habitacion2.ponerSur(self.fabricarPared())
 
+    #Creación del laberinto esquematizado para la entrega
+    def laberinto4habitaciones(self):
+        self.laberinto = self.fabricarLaberinto()
+
+        habitacion1 = self.fabricarHabitacion(1)
+        habitacion2 = self.fabricarHabitacion(2)
+        habitacion3 = self.fabricarHabitacion(3)
+        habitacion4 = self.fabricarHabitacion(4)
+        puerta1 = self.fabricarPuerta(habitacion1, habitacion2)
+        puerta2 = self.fabricarPuerta(habitacion2, habitacion4)
+        puerta3 = self.fabricarPuerta(habitacion3, habitacion4)
+        puerta4 = self.fabricarPuerta(habitacion1, habitacion3)
+
+        habitacion1.ponerNorte(self.fabricarPared())
+        habitacion1.ponerOeste(self.fabricarPared())
+        habitacion1.ponerEste(puerta1)
+        habitacion1.ponerSur(puerta4)
+
+        habitacion2.ponerNorte(self.fabricarPared())
+        habitacion2.ponerEste(self.fabricarPared())
+        habitacion2.ponerOeste(puerta1)
+        habitacion2.ponerSur(puerta2)
+
+        habitacion3.ponerOeste(self.fabricarPared())
+        habitacion3.ponerSur(self.fabricarPared())
+        habitacion3.ponerEste(puerta3)
+        habitacion3.ponerNorte(puerta4)
+
+        habitacion4.ponerEste(self.fabricarPared())
+        habitacion4.ponerSur(self.fabricarPared())
+        habitacion4.ponerNorte(puerta2)
+        habitacion4.ponerOeste(puerta3)
+        
 
     def fabricarHabitacion(self, unNum):
         habitacion = habitacion (unNum)
