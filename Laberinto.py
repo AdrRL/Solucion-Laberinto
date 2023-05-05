ed0235ce8552e55ff82c5e3d277d1a98f6e08a7a
ADRIÁN RODRÍGUEZ LÓPEZ <Adrian.Rodriguez18@alu.uclm.es>
2023-05-06 01:40:46 +0200

    Arreglo de clase Laberinto

--- Laberinto.py
+++ Laberinto.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-class Laberinto:
+from Contenedor import Contenedor
+from Habitacion import Habitacion
 
-    def __init__(self, nHabitaciones):
-        self.habitaciones = 
+class Laberinto(Contenedor):
+
+    def __init__(self, num):
+        super().__init__(num)
+
+    def agregarHabitacion(self, unaHab):
+        self.agregarHijo(unaHab)
 
     def numeroHabitaciones(self):
-        return self.
+        contador = 0
+        for hijo in self.hijos:
+            if hijo.esHabitacion:
+                contador += 1
+        return contador
 
+    def obtenerHabitacion(self, unNum):
+        for hijo in self.hijos:
+            if hijo.esHabitacion:
+                if hijo.num == unNum:
+                    return hijo
